0d353d0becd8203b3a5d734b95e41f894b2b6fa8
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-03-21 23:23:52 +0530

    Create 300. Longest Increasing Subsequence.cpp

---
+++ 300. Longest Increasing Subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        // O(n^2)
+        int n = nums.size();
+        vector<int>dp(n);
+        dp[0] = 1;
+        for(int i=1;i<n;i++)
+        {
+            dp[i]=1;
+            for(int j=0;j<i;j++)
+            {
+                if(nums[j]<nums[i] && dp[i]<dp[j]+1)
+                {
+                    dp[i]=dp[j]+1;
+                }
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
