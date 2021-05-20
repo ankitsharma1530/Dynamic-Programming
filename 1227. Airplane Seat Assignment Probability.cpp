192ca30c6c0ef34ea60b1cf707d04fd396d439a5
Ankit Sharma <77480735+ankitsharma1530@users.noreply.github.com>
2021-05-20 13:25:03 +0530

    Create 1227. Airplane Seat Assignment Probability.cpp

---
+++ 1227. Airplane Seat Assignment Probability.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1)
+        {
+            return (double)1;
+        }
+        else 
+        {
+            return (double)1/2;
+        }
+    }
+};
